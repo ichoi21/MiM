c0fa140ceafe16d1698bdcc1fe2f13d922cecc4e
myhkas <mmyhkas@gmail.com>
2020-11-28 12:07:02 -0800

    added more objects to /quote request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,14 @@ router.post("/quote", async (req,res) => {
         let result = {}
         let symbol = req.body.symbol
       await Axios.get(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${process.env.FIN_TOKEN}`)
-      .then((res) => result = res.data)
+      .then((res)=> result.quote = res.data)
+      await Axios.get(`https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${process.env.FIN_TOKEN}`)
+      .then((res) => result.profile = res.data)
+      await Axios.get(`https://finnhub.io/api/v1/company-news?symbol=${symbol}&from=2020-04-30&to=2020-05-01&token=${process.env.FIN_TOKEN}`)
+      .then((res) => result.news = res.data)
+      await Axios.get(`https://finnhub.io/api/v1 /stock/candle?symbol=${symbol}&resolution=1&from=1605543327&to=1605629727&token=${process.env.FIN_TOKEN}`)
+      .then((res) => result.candles = res.data)
+      
       res.send(result)
     }
     catch(err){
