1f01e3b7f3bf14b32f9c525840e0fd032afdd63f
myhkas <mmyhkas@gmail.com>
2020-11-28 09:24:47 -0800

    fixed duplicated apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ router.post("/quote", async (req,res) => {
     try{
         let result = {}
         let symbol = req.body.symbol
-      await Axios.get(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=bv17naf48v6p0f6icqr0`)
+      await Axios.get(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${process.env.FIN_TOKEN}`)
       .then((res) => result = res.data)
       res.send(result)
     }
