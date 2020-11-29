9fe875b4c4365d7bd2cdb5864707747f0d2b0ecf
Michelle <mptran0101@berkeley.edu>
2020-11-28 21:34:59 -0800

    added finnhub axios and table

---
+++ routes/apiRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const Axios = require("axios");
+const auth = require("../middleware/auth");
+//const ProfileRouter = express.Router();
+
+//load Profile
+
+//load FinHub
