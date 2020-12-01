718b20fbd1f61786e231bce7110cbc89b42feacc
myhkas <mmyhkas@gmail.com>
2020-12-01 07:33:21 -0800

    added searchbar comp, not working atm

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,17 +1,38 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import UserContext from "../Context/UserContext";
-
+import SearchBar from "../Components/SearchBar";
+import Axios from "axios";
 
 const Home = () => {
   const { userData, setUserData } = useContext(UserContext);
   const history = useHistory();
 
+const [search, setSearch] = useState("");
+const [quote, setQuote] = useState([]);
+
+const getQuote = (e) =>{
+    e.preventDefault();
+    Axios.get(
+        `/user/quote`
+      ).then((res) => {
+        setQuote(res.data);
+        console.log(res.data);
+      });
+    }
+    
+
+
   useEffect(() => {
     if (!userData.user) history.push("/login");
   }, [userData.user, history]);
 
-  return <h1>dMiM $tock search</h1>;
+  return (
+    <>
+  <h1>dMiM $tock search</h1>
+  <SearchBar onChange={(e) => setSearch(e.target.value)} onSubmit={()=> getQuote()}/>
+  </>
+    );
 };
 
 export default Home;
