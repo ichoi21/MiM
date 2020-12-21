67b3e6af045a61d7bcf8fbf0dc5701789c6b647e
myhkas <mmyhkas@gmail.com>
2020-12-20 23:02:28 -0800

    added Header with working navigation. added pages as well

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,23 +1,72 @@
-import React, { useEffect, useState } from "react";
-import { Grid, Link, makeStyles, Typography } from "@material-ui/core";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import App from "../../App"
+import React,{ useContext, useEffect, useState } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { useHistory } from "react-router-dom";
+import UserContext from "../../Context/UserContext";
+import Axios from "axios";
 
-const Header = () => {
-  // const classes = useStyles();
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+
+ const Header=()=> {
+  const {userData, setUserData} = useContext(UserContext);
+  const history = useHistory();
+  const scanner = () => history.push("/scanner");
+  const callresults = () => history.push("/callresults");
+  const home = () => history.push("/");
+  const settings = () => history.push("/settings");
+  const signup = () => history.push("/signup");
+  const logout = () => {
+    setUserData({
+      token: undefined,
+      user: undefined,
+    });
+    history.push("/");
+    localStorage.setItem("auth-token", "");
+  };
+  const classes = useStyles();
+
   return (
-    <BrowserRouter>
-<div>
-      {/* <Link to="/signup">Signup</Link>
-      <Link to="/login">Login</Link> */}
-      <Link to="/login">Dashboard</Link>
-      <Link to="/login">Scanner</Link>
-      <Link to="/login">Daily Calls/Results</Link>
-      <Link to="../../Pages/login" onClick={App.logout}>Logout</Link>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} onClick={home}>
+            MiM
+          </Typography>
+          {userData.user ? (
+            <>
+            <Button color="inherit" onClick={scanner}>Scanner</Button>
+            <Button color="inherit" onClick={callresults}>Daily Calls/Results</Button>
+            <Button color="inherit" onClick={settings}>Settings</Button>
+            <Button color="inherit" onClick={logout}>Logout</Button>
 
-  </div>
-</BrowserRouter>
+            </>
+          ):(
+            <>
+            <Button color="inherit" onClick={logout}>Login</Button> 
+            <Button color="inherit" onClick={signup}>Sign Up</Button> 
+            </>
+          )}
+         
+        </Toolbar>
+      </AppBar>
+    </div>
   );
-};
-
+}
 export default Header;
---
+++ client/src/Pages/Callresults.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Callresults() {
+    return (
+        <div>
+            <h1>Calls and Results</h1>
+        </div>
+    )
+}
+
+export default Callresults
