7f240b6bf1b3212d64311124b5d1c7d7414bc149
ichoi21 <ihchoi@gmail.com>
2021-01-02 21:13:23 -0800

    Added placeholder for Disclaimer

---
+++ client/src/Pages/Disclaimer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../Components/Footer'
+
+function Disclaimer() {
+  return (
+    <div>
+    <h1>Disclaimer</h1>
+    <Footer />
+    </div>
+  )
+}
+
+export default Disclaimer
