9804894eb61b2f0fccb23d3d37ffa06bfca43424
ichoi21 <ihchoi@gmail.com>
2021-01-18 12:02:03 -0800

    revised mobile app to look like an app

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,9 +1,11 @@
-import { AppBar, Button, Drawer, Hidden, List, ListItem, ListItemText, Toolbar } from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
+import { AppBar, Button, Drawer, Hidden, List, ListItem, ListItemText, Toolbar, Typography, IconButton, BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import CloseIcon from "@material-ui/icons/Close";
-import ExitToApp from "@material-ui/icons/ExitToApp";
+import ExitToApp from '@material-ui/icons/ExitToApp';
+import CancelIcon from '@material-ui/icons/Cancel';
+import HomeIcon from '@material-ui/icons/Home';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import PageviewIcon from '@material-ui/icons/Pageview';
 import React,{ useContext, useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 
@@ -14,44 +16,40 @@ import { Date, Time } from "../Time/"
 //styles
 import "./Header.css";
 
-const drawerWidth = 240;
+
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
     flexGrow: 1,
     top: 0,
     maxHeight: "8vh",
     minHeight: "5vh",
     boxShadow: "none",
     backgroundColor: "#343a38",
-    width: "auto",
+  },
+  mroot: {
+    width: 500,
+    backgroundColor: "#343a38",
   },
   drawer: {
     [theme.breakpoints.up("sm")]: {
-      width: drawerWidth,
+      width: "auto",
       flexShrink: 0,
+      backgroundColor: "#343a38",
     },
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up("sm")]: {
-      display: "none",
-    },
-  },
   toolbar: theme.mixins.toolbar,
   drawerPaper: {
-    width: drawerWidth,
+    width: "flex",
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
   },
-  closeMenuButton: {
-    marginRight: "auto",
-    marginLeft: 0,
+  menuButton: {
+    marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
@@ -65,7 +63,6 @@ const useStyles = makeStyles((theme) => ({
   const dsp = () => history.push("/dsp");
   const home = () => history.push("/");
   const portfolio = () => history.push("/portfolio");
-  // const signup = () => history.push("/signup");
   const logout = () => {
     setUserData({
       token: undefined,
@@ -76,24 +73,16 @@ const useStyles = makeStyles((theme) => ({
   };
 
   const sectionLinks = [
-    // {userData.user ? (
-    //   <>
       <Button color="inherit" onClick={portfolio}>Portfolio</Button>,
       <Button color="inherit" onClick={scanner}>Scanner</Button>,
       <Button color="inherit" onClick={dsp}>1DSP</Button>,
       <Button color="inherit" onClick={logout}>Logout<ExitToApp /></Button>
-    //   </>
-    // ):(
-    //   <>
-    //   <Button color="inherit" onClick={logout}>Login</Button> 
-    //   <Button color="inherit" onClick={signup}>Sign Up</Button> 
-    //   </>
-    // )}
   ];
 
   const classes = useStyles();
   const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  
   function handleDrawerToggle() {
     setMobileOpen(!mobileOpen);
   }
@@ -101,14 +90,16 @@ const useStyles = makeStyles((theme) => ({
     <div>
       <List className="childList">
         <ListItem>
-        <Typography variant="h7" className={classes.title}>
-              <Button color="inherit" onClick={home}>
+        <Typography variant="caption">
+              <Link color="inherit" onClick={home} edge="start" className={classes.menuButton}>
                 <img src={logo} className="logo"/>
-              </Button>
+              </Link>
             </Typography>
         </ListItem>
-        <ListItem>NYSE&nbsp;<Date timeZone="America/New_York"/>&nbsp;
-        <Time timeZone="America/New_York"/></ListItem>
+        <ListItem variant="caption">
+          NYSE&nbsp;<Date timeZone="America/New_York"/>&nbsp;
+        <Time timeZone="America/New_York"/>
+        </ListItem>
         {sectionLinks.map((text, index) => (
           <ListItem button key={text}>
             <ListItemText primary={text} />
@@ -118,50 +109,31 @@ const useStyles = makeStyles((theme) => ({
     </div>
   );
 
-  const Mdrawer = (
-    <div>
-      <List>
-        {sectionLinks.map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-        <ListItem>NYSE&nbsp;<Date timeZone="America/New_York"/></ListItem>
-        <ListItem><Time timeZone="America/New_York"/></ListItem>
-      </List>
-    </div>
+  const mappBar = (
+ <BottomNavigation className={classes.mroot}>
+   <BottomNavigationAction label="Home" onClick={home} icon={<HomeIcon />}/>
+   <BottomNavigationAction label="Portfolio" onClick={portfolio} icon={<AssignmentIcon />}/>
+   <BottomNavigationAction label="Scanner" onClick={scanner} icon={<PageviewIcon />}/>
+   <BottomNavigationAction label="1DSP" onClick={dsp} icon={<TrendingUpIcon />}/>
+ </BottomNavigation>
   );
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" className={classes.appBar}>
-        <Toolbar className="parentList">
-          <IconButton 
-            color="inherit" 
-            aria-label="Dropdown Menu" 
-            edge="start" 
-            onClick={handleDrawerToggle} 
-            className={classes.menuButton}>
-            <img className="logo" src={logo} alt="Menu" />
-          </IconButton>
-          <Hidden xsDown implementation="css">
+      <Hidden xsDown implementation="css">
+        <AppBar position="static" className={classes.appBar}>
+          <Toolbar className="parentList">
             {drawer}
-          </Hidden>
-        </Toolbar>
-      </AppBar>
+          </Toolbar>
+        </AppBar>
+      </Hidden>
       <nav className={classes.drawer}>
         <Hidden smUp implementation="css">
           <Drawer
-            variant="temporary"
-            anchor={theme.direction === "rtl" ? "right" : "left"}
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            classes={{ paper: classes.drawerPaper, }}
-            ModalProps={{ keepMounted: true, }}>
-            <IconButton onClick={handleDrawerToggle} className={classes.closeMenuButton}>
-              <CloseIcon />
-            </IconButton>
-            {Mdrawer}
+            variant="permanent"
+            anchor="bottom"
+            classes={{ paper: classes.drawerPaper, }}>
+            {mappBar}
           </Drawer>
         </Hidden>
         <Hidden xlDown implementation="css">
