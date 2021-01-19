022cfa6712b7e66e78a07fa961f9eb0317c9f175
ichoi21 <ihchoi@gmail.com>
2021-01-18 18:02:50 -0800

    standardize styles for sectors

--- client/src/Components/Sector/index.js
+++ client/src/Components/Sector/index.js
@@ -1,9 +1,24 @@
 import React, {useEffect, useState} from 'react';
-import { Grid, Link, makeStyles, spacing, Typography } from "@material-ui/core";
-import { MDBListGroup, MDBListGroupItem, MDBContainer } from "mdbreact";
+import { Grid, List, ListItem, Card, makeStyles, Typography } from "@material-ui/core";
 import Axios from 'axios';
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 50,
+    minHeight: 100,
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 12,
+    color: "#fffff0",
+  },
+  pos: {
+    marginBottom: 5,
+  },
+});
+
 const Sector = () => {
+const classes = useStyles();
 const [sector, setSector] = useState([]);
 
 const getSector = () => {
@@ -15,16 +30,20 @@ useEffect(() => {
 }, [])
 
 return (
-  <Grid container >
+  <Grid container justify="center" spacing={1}>
   {sector.map((item) => {
     return (
-    <Grid item xs={3}>
-    <MDBListGroup style={{ width: "16rem" }}>
-      <MDBListGroupItem href={item.url} hover>
-          <h9>{item.name}</h9>
-          <p>{(item.performance*100).toFixed(2)}%</p>
-      </MDBListGroupItem>
-    </MDBListGroup>
+    <Grid item xs={2}>
+    <List>
+      <Card className={classes.root} elevation={3}>
+      <ListItem button key={item.url}>
+        <Typography className={classes.title}>{item.name}</Typography>
+      </ListItem>
+      <ListItem>
+        <Typography className={classes.pos} variant="caption">{(item.performance*100).toFixed(2)}%</Typography>
+      </ListItem>
+      </Card>
+    </List>
     </Grid>
     )
   })}
